81618020e93782c47050ffde735d799933c2375e
Gael Partida <gaelarthpart@gmail.com>
2026-03-03 05:01:28 -0700

    First Update to the Demo App

--- HomieAppPreview/app/src/main/java/com/example/homieapppreview/MainActivity.kt
+++ HomieAppPreview/app/src/main/java/com/example/homieapppreview/MainActivity.kt
@@ -4,13 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.homieapppreview.ui.theme.HomieAppPreviewTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +44,148 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HomieAppPreviewTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomieAppPreviewUI()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun HomieAppPreviewUI() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        HomieAppPreviewLayout(
+            true,
+            32,
+            60,
+            300
+        )
+    }
+}
+@Composable
+fun HomieAppPreviewLayout(connected: Boolean, temperature: Int, humidity: Int, airQuality: Int) {
+    var temperatureData = "N/A"
+    var humidityData = "N/A"
+    var airQualityData = "N/A"
+
+    if (connected) {
+        temperatureData = "${temperature.toString()}°C"
+        humidityData = "${humidity.toString()}%"
+        airQualityData = "${airQuality.toString()}ppm"
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(
+            top = 48.dp,
+            start = 32.dp,
+            end = 32.dp,
+        )
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(bottom = 16.dp)
+        ) {
+            PrincipalText(
+                text = "Homie Mobile",
+                size = 32,
+            )
+            Row() {
+                if (connected) {
+                    Text(
+                        text = "Connected",
+                        color = colorResource(R.color.green_homie),
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                } else {
+                    Text(
+                        text = "Disconnected",
+                        color = colorResource(R.color.alert_color),
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(bottom = 8.dp, top = 8.dp)
+        ) {
+            DataRow(
+                imageVector = Icons.Filled.Home,
+                type = "Temperature: ",
+                data = temperatureData,
+                color = R.color.blue_homie
+            )
+            DataRow(
+                imageVector = Icons.Filled.Settings,
+                type = "Humidity: ",
+                data = humidityData,
+                color = R.color.blue_homie
+            )
+            DataRow(
+                imageVector = Icons.Filled.Warning,
+                type = "Air Quality: ",
+                data = airQualityData,
+                color = R.color.blue_homie
+            )
+        }
+    }
+}
+
+@Composable
+fun PrincipalText(text: String, size: Int) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = text,
+        color = MaterialTheme.colorScheme.primary,
+        fontSize = size.sp,
+        fontWeight = FontWeight.SemiBold
     )
+}
+
+@Composable
+fun SecondaryText(text: String, size: Int) {
+    Text(
+        text = text,
+        color = MaterialTheme.colorScheme.secondary,
+        fontSize = size.sp,
+        fontWeight = FontWeight.Normal
+    )
+}
+
+@Composable
+fun DataRow(imageVector: ImageVector, type: String, data: String, color: Int) {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(bottom = 8.dp, top = 8.dp)
+            .height(56.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(MaterialTheme.colorScheme.surface),
+        verticalAlignment = Alignment.CenterVertically,
+        ) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = null,
+            tint = Color(color),
+            modifier = Modifier.size(64.dp).padding(start = 8.dp, end = 8.dp)
+        )
+        SecondaryText(
+            text = type,
+            size = 28
+        )
+        PrincipalText(
+            text = data,
+            size = 28
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomieAppPreviewPreview() {
     HomieAppPreviewTheme {
-        Greeting("Android")
+        HomieAppPreviewUI()
     }
 }
--- HomieAppPreview/app/src/main/java/com/example/homieapppreview/ui/theme/Theme.kt
+++ HomieAppPreview/app/src/main/java/com/example/homieapppreview/ui/theme/Theme.kt
@@ -10,34 +10,29 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import com.example.homieapppreview.R
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+@Composable
+private fun darkColorSchemeFromXml() = darkColorScheme(
+    background = colorResource(id = R.color.dark_background),
+    surface = colorResource(id = R.color.dark_surface),
+    primary = colorResource(id = R.color.dark_primary_text),
+    secondary = colorResource(id = R.color.dark_secondary_text),
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+@Composable
+private fun lightColorSchemeFromXml() = lightColorScheme(
+    background = colorResource(id = R.color.light_background),
+    surface = colorResource(id = R.color.light_surface),
+    primary = colorResource(id = R.color.light_primary_text),
+    secondary = colorResource(id = R.color.light_secondary_text),
 )
 
 @Composable
 fun HomieAppPreviewTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -46,8 +41,8 @@ fun HomieAppPreviewTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> darkColorSchemeFromXml()
+        else -> lightColorSchemeFromXml()
     }
 
     MaterialTheme(
